d92fc907827261f6716766bf8320b59c4defe4e9
markdeep98 <dmitriy.gladky98@gmail.com>
2018-05-08 07:01:57 -0700

    Update

--- vendor/config/routes.rb
+++ vendor/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
- 
-  devise_for :users, :controllers => { registrations: 'registrations' }
-	root to: "pages#home"
+ 	
+  	devise_for :users, :controllers => { registrations: 'registrations' }
+	
+	resources :posts
+
+	authenticated :user do
+		root "pages#home", as: :authenticated_root
+	end
+
+	root :to => redirect("/users/sign_in")
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
